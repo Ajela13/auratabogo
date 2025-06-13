b0714e2c3768df0f5f23e555bb6aa928a481e284
Ajela13 <alejamarino123@gmail.com>
2025-06-12 22:16:07 -0500

    Feat: Home page created

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+    <div className="relative flex flex-col items-center justify-center h-[70vh]  bg-gray-200 w-[90vw] m-auto shadow-lg rounded-lg">
+      <img src="/src/assets/logo.svg" alt="Hero" className="absolute -top-20 w-200 h-60 object-scale-down " />
+    </div>)
+  };
+  
+  export default Home;
+  

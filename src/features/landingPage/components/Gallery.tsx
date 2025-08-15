9bbe43230e1a00f61c7cb19734fbdc669a91a46b
Alejamarino <amarino@visualcontact.com.co>
2025-08-15 12:19:27 -0500

    Feat: add spotify content

--- src/features/landingPage/components/Gallery.tsx
+++ src/features/landingPage/components/Gallery.tsx
@@ -63,6 +63,8 @@ const Gallery = () => {
           ))}
         </Swiper>
       </div>
+      <iframe data-testid="embed-iframe" className="mt-10 border-radius:12px" src="https://open.spotify.com/embed/artist/3jSaEwHt3xkuS6Sm9oY6GZ?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
     </div>
   );
 };

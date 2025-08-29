91c5dc20de0a05af7a1fd31100479f17d286293f
Alejamarino <amarino@visualcontact.com.co>
2025-08-29 16:09:52 -0500

    Style: adjust logo and navbar

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,31 +1,40 @@
+import logo from "../assets/logo.svg";
+
 const Navbar = () => {
   return (
-    <nav className=' font-montserrat text-white p-8 '>
-      <ul className="flex justify-center space-x-4">
-        <li>
-          <a href="#home" className="m-2 text-lg ">
+    <nav className='font-montserrat text-white fixed w-full z-10 bg-opacity-50 flex'>
+      <div className="flex items-center justify-between text-xl">
+        <a href="#home" className="m-2 text-lg">
+              <img alt="Logo" src={logo} className="animate-pulse h-45 object-scale-down  "  />
+        </a>
+      </div>
+      <ul className="flex center justify-center space-x-4 text-lg bg-black/90 rounded-full py-5 px-3 m-auto">
+        <li className="hover:border-b-2 border-white rounded-s">
+          <a href="#home" className="m-2  ">
           Home
           </a>
         </li>
-        <li>
-          <a href="#gallery" className="m-2 text-lg ">
+        <li className="hover:border-b-2 border-white rounded-s">
+          <a href="#gallery" className="m-2 ">
             Gallery
           </a>
         </li>
-        <li>
-          <a href="#bio" className="m-2 text-lg ">
+        <li className="hover:border-b-2 border-white rounded-s">
+          <a href="#bio" className="m-2  ">
             Biography
           </a>
         </li>
-        
-        <li>
-          <a href="#contact" className="m-2 text-lg">
-            Contact
+
+        <li className="hover:border-b-2 border-white rounded-s">
+          <a href="#contact" className="m-2 ">
+            Follow me
           </a>
         </li>
-      </ul>
 
-    </nav>
+        
+      </ul>
+      
+      </nav>
   );
 };
 

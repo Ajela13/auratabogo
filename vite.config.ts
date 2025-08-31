d03750cfd3120e0de2e81327cae47c9695120bde
Alejamarino <amarino@visualcontact.com.co>
2025-08-30 19:46:29 -0500

    Config: github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(),react()],
+  base: '/auratabogo/',
   resolve: {
     alias: {
       // 3. Tu alias ahora funcionará correctamente

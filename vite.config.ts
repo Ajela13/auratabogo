375578fe717fce5ed7850d8a836fff1601845d09
Alejamarino <amarino@visualcontact.com.co>
2025-09-05 10:48:00 -0500

    Fix: change base path custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(),react()],
-  base: '/auratabogo/',
+  base: './',
   resolve: {
     alias: {
       // 3. Tu alias ahora funcionará correctamente
